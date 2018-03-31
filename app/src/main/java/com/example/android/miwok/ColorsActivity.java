b90d149a7efafc4b5311ab1d04bcd163845f38ae
fahmifan <fahmiirfan909@gmail.com>
2018-03-31 10:00:43 +0700

    Add audio for every words

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -15,32 +15,67 @@
  */
 package com.example.android.miwok;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class ColorsActivity extends AppCompatActivity {
+
+    private MediaPlayer mMediaPlayer = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.word_list);
 
-        ArrayList<Word> words = new ArrayList<Word>();
+        final ArrayList<Word> words = new ArrayList<Word>();
 
-        words.add(new Word("red", "wetetti", R.drawable.color_red));
-        words.add(new Word("mustard yellow", "chiwiita", R.drawable.color_mustard_yellow));
-        words.add(new Word("dusty yellow", "topiise", R.drawable.color_dusty_yellow));
-        words.add(new Word("green", "chokokki", R.drawable.color_green));
-        words.add(new Word("brown", "takaakki", R.drawable.color_brown));
-        words.add(new Word("gray", "topoppi", R.drawable.color_gray));
-        words.add(new Word("black", "kulullli", R.drawable.color_black));
-        words.add(new Word("white", "kulelli", R.drawable.color_white));
+        words.add(new Word("red", "wetetti", R.drawable.color_red, R.raw.color_red));
+        words.add(new Word("mustard yellow", "chiwiita", R.drawable.color_mustard_yellow, R.raw.color_mustard_yellow));
+        words.add(new Word("dusty yellow", "topiise", R.drawable.color_dusty_yellow, R.raw.color_dusty_yellow));
+        words.add(new Word("green", "chokokki", R.drawable.color_green, R.raw.color_green));
+        words.add(new Word("brown", "takaakki", R.drawable.color_brown, R.raw.color_brown));
+        words.add(new Word("gray", "topoppi", R.drawable.color_gray, R.raw.color_gray));
+        words.add(new Word("black", "kulullli", R.drawable.color_black, R.raw.color_black));
+        words.add(new Word("white", "kulelli", R.drawable.color_white, R.raw.color_white));
 
         ListView listView = (ListView) findViewById(R.id.list);
         WordAdapter itemsAdapter = new WordAdapter(this, words, R.color.category_colors);
         listView.setAdapter(itemsAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                if(words.get(position).hasAudio()){
+                    // Release any audio player if there is still take resource
+                    releaseMediaPlayer();
+                    mMediaPlayer = MediaPlayer.create(ColorsActivity.this, words.get(position).getmAudioResourceId());
+                    mMediaPlayer.start();
+                    mMediaPlayer.setOnCompletionListener(mCompletionListener);
+                } else {
+                    Toast.makeText(ColorsActivity.this, "No Audio", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mediaPlayer) {
+            releaseMediaPlayer();
+        }
+    };
+
+    private void releaseMediaPlayer() {
+        if(mMediaPlayer != null) {
+            mMediaPlayer.release();
+            mMediaPlayer = null;
+        }
     }
 }
--- app/src/main/java/com/example/android/miwok/PhrasesActivity.java
+++ app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -15,34 +15,68 @@
  */
 package com.example.android.miwok;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class PhrasesActivity extends AppCompatActivity {
+    private MediaPlayer mMediaPlayer = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.word_list);
 
-        ArrayList<Word> words = new ArrayList<Word>();
+        final ArrayList<Word> words = new ArrayList<Word>();
 
-        words.add(new Word("Where are you going?", "minto wuksus"));
-        words.add(new Word("What is your name?", "tinna oyaase'na"));
-        words.add(new Word("My name is...", "oyaaset..."));
-        words.add(new Word("How are you feeling?", "michaksas?"));
-        words.add(new Word("I'm feeling good.", "kuchi achit"));
-        words.add(new Word("Are you coming?", "aanas'aa?"));
-        words.add(new Word("Yes, I'm coming", "haa' aanam"));
-        words.add(new Word("I'm coming.", "aanam"));
-        words.add(new Word("Let's go.", "yoowutis"));
-        words.add(new Word("Come here.", "anni'nem"));
+        words.add(new Word("Where are you going?", "minto wuksus", R.raw.phrase_where_are_you_going));
+        words.add(new Word("What is your name?", "tinna oyaase'na", R.raw.phrase_what_is_your_name));
+        words.add(new Word("My name is...", "oyaaset...", R.raw.phrase_my_name_is));
+        words.add(new Word("How are you feeling?", "michaksas?", R.raw.phrase_how_are_you_feeling));
+        words.add(new Word("I'm feeling good.", "kuchi achit", R.raw.phrase_im_feeling_good));
+        words.add(new Word("Are you coming?", "aanas'aa?", R.raw.phrase_are_you_coming));
+        words.add(new Word("Yes, I'm coming", "haa' aanam", R.raw.phrase_yes_im_coming));
+        words.add(new Word("I'm coming.", "aanam", R.raw.phrase_im_coming));
+        words.add(new Word("Let's go.", "yoowutis", R.raw.phrase_lets_go));
+        words.add(new Word("Come here.", "anni'nem", R.raw.phrase_come_here));
 
         ListView listView = (ListView) findViewById(R.id.list);
         WordAdapter itemsAdapter = new WordAdapter(this, words, R.color.category_phrases);
         listView.setAdapter(itemsAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                if(words.get(position).hasAudio()){
+                    // Release any audio player if there is still take resource
+                    releaseMediaPlayer();
+                    mMediaPlayer = MediaPlayer.create(PhrasesActivity.this, words.get(position).getmAudioResourceId());
+                    mMediaPlayer.start();
+                    mMediaPlayer.setOnCompletionListener(mCompletionListener);
+                } else {
+                    Toast.makeText(PhrasesActivity.this, "No Audio", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mediaPlayer) {
+            releaseMediaPlayer();
+        }
+    };
+
+    private void releaseMediaPlayer() {
+        if(mMediaPlayer != null) {
+            mMediaPlayer.release();
+            mMediaPlayer = null;
+        }
     }
 }
